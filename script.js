1abfa71145d4820d913a402109642a48fd8a5f44
deepika <janadipika17@gmail.com>
2025-12-20 23:40:49 +0530

    3rd commit

--- script.js
+++ script.js
@@ -1,78 +1,47 @@
-function toggleMenu(){
-  const menu=document.getElementById("menu");
-  menu.style.display=menu.style.display==="flex"?"none":"flex";
-}
-
-function shareReferral(){
-  const code=document.getElementById("referralCode").innerText;
-  navigator.clipboard.writeText(code);
-  alert("Referral code copied!");
-}
-
-/* DATA */
-const data={
-  exam:{
-    ssc:[
-      {name:"SSC Math",author:"A. Das",price:299,img:"images/book1.jpg"},
-      {name:"SSC English",author:"B. Roy",price:249,img:"images/book2.jpg"}
-    ]
-  },
-  school:{
-    class10:[
-      {name:"Class 10 Science",author:"NCERT",price:199,img:"images/book3.jpg"}
-    ]
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+  if(menu.style.display === "flex") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "flex";
   }
-};
-
-function loadSubcategory(){
-  const cat=category.value;
-  subcategory.innerHTML="<option value=''>Select Subcategory</option>";
-
-  if(cat==="exam") subcategory.innerHTML+=`<option value="ssc">SSC</option>`;
-  if(cat==="school") subcategory.innerHTML+=`<option value="class10">Class 10</option>`;
 }
 
-function loadBooks(){
-  const cat=category.value;
-  const sub=subcategory.value;
-  const list=document.getElementById("bookList");
-  list.innerHTML="";
 
-  data[cat][sub].forEach(book=>{
-    list.innerHTML+=`
-      <div class="product-card">
-        <img src="${book.img}">
-        <h4>${book.name}</h4>
-        <p>${book.author}</p>
-        <p class="price">₹${book.price}</p>
-        <div class="card-btns">
-          <button class="buy" onclick="openBuyModal('${book.name}')">Buy Now</button>
-          <button class="demo">PDF Demo</button>
-        </div>
-      </div>
-    `;
-  });
+function openLogin(){
+  document.getElementById("loginPopup").style.display = "flex";
+  document.getElementById("registerPopup").style.display = "none";
 }
 
-function searchBook(){
-  const val=bookSearch.value.toLowerCase();
-  document.querySelectorAll(".product-card").forEach(card=>{
-    card.style.display=card.innerText.toLowerCase().includes(val)?"block":"none";
-  });
+function closeLogin(){
+  document.getElementById("loginPopup").style.display = "none";
 }
 
-function openBuyModal(book){
-  selectedBook.innerText=book;
-  buyModal.style.display="block";
-}
-function closeBuyModal(){buyModal.style.display="none"}
-
-function toggleAddress(){
-  const type=document.querySelector("input[name='buyType']:checked").value;
-  addressForm.style.display=type==="hardcopy"?"block":"none";
+function openRegister(){
+  closeLogin();
+  document.getElementById("registerPopup").style.display = "flex";
 }
 
-function proceedPayment(){
-  alert("Order placed for "+selectedBook.innerText);
-  closeBuyModal();
+function closeRegister(){
+  document.getElementById("registerPopup").style.display = "none";
+}
+
+function login(){
+  const u = document.getElementById("loginUser").value;
+  const p = document.getElementById("loginPass").value;
+
+  if(u === "admin@gmail.com" && p === "123456"){
+    localStorage.setItem("gmLogin","true");
+    closeLogin();
+    alert("Login Successful");
+  }else{
+    document.getElementById("loginError").innerText =
+      "Wrong User ID or Password";
+  }
+}
+
+function register(){
+  alert("Registration Successful! Please Login.");
+  closeRegister();
+  openLogin();
 }
